cbd6955418987fa2205fdb77be668b0fc8b75af6
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2021-12-20 19:20:40 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className={'text-blue-500'}>
+        About
+      </div>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className={'text-red-500'}>
+        Homepage
+      </div>
+    </div>
+  )
+}
